404cd45b705dc1c013e44f6a7abdd1d8bbc51f36
pricelees <priceelees@gmail.com>
2024-08-30 14:20:14 +0900

    google analytics 등록을 위한 tag 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
 
   plugins: [
     {
+      resolve: 'gatsby-plugin-gtag',
+      options: {
+        trackingId: 'G-HJPBBDEJZ7',
+        head: true,
+        anonymize: true,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
